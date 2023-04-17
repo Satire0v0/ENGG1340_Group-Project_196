90b8ccbf0a58a6c5e0d5644bf296715b77a662f7
ianwu0907 <125888851+ianwu0907@users.noreply.github.com>
2023-04-17 14:37:20 +0800

    Add files via upload
    an example of the game files

---
+++ paperscissor.h
@@ -0,0 +1,9 @@
+#ifndef PAPERSCISSOR_H
+#define PAPERSCISSOR_H
+//header file od rock paper scissors game
+//rpsgame returns an integer value representing the winning status of the game
+//if there is a draw, the game will continue untill the player wins or loses 
+
+using namespace std;
+int rpsgame();
+#endif
