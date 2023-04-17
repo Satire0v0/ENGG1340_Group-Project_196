9ffb9a3d794f113969e4a0d30e9168f97fc5b855
ianwu0907 <125888851+ianwu0907@users.noreply.github.com>
2023-04-17 21:02:02 +0800

    Delete paperscissor.h

--- paperscissor.h
+++
@@ -1,9 +0,0 @@
-#ifndef PAPERSCISSOR_H
-#define PAPERSCISSOR_H
-//header file od rock paper scissors game
-//rpsgame returns an integer value representing the winning status of the game
-//if there is a draw, the game will continue untill the player wins or loses 
-
-using namespace std;
-int rpsgame();
-#endif
