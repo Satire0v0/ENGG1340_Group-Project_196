219f7b290ad5c231ae60bcaab741023968c0d0aa
ianwu0907 <125888851+ianwu0907@users.noreply.github.com>
2023-04-17 21:05:06 +0800

    Add files via upload

---
+++ game.h
@@ -0,0 +1,11 @@
+#ifndef Game_H
+#define Game_H
+
+//rpsgame returns an integer value representing the winning status of the game
+//if there is a draw, the game will continue untill the player wins or loses 
+
+using namespace std;
+bool rpsgame();
+bool guess_die();
+bool number_guess();
+#endif
